d970331728f58dc93c571e8f67c4407d825a7e5b
lomayd <jinho10167@gmail.com>
2022-10-11 01:40:32 +0900

    feat: Project initialize

---
+++ src/main/java/dcom/ssdbackend/api/SsdBackendApplication.java
@@ -0,0 +1,13 @@
+package dcom.ssdbackend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdBackendApplication.class, args);
+	}
+
+}
