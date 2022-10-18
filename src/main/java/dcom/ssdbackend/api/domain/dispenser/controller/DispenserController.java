9f51f3e8b20ac3ac5712c92ae99ca014de3aa007
lomayd <jinho10167@gmail.com>
2022-10-19 07:38:14 +0900

    feat: Add Dispenser CRUD REST API

--- src/main/java/dcom/ssdbackend/api/domain/dispenser/controller/DispenserController.java
+++ src/main/java/dcom/ssdbackend/api/domain/dispenser/controller/DispenserController.java
@@ -1,4 +1,45 @@
 package dcom.ssdbackend.api.domain.dispenser.controller;
 
+import dcom.ssdbackend.api.domain.dispenser.dto.DispenserResponseDto;
+import dcom.ssdbackend.api.domain.dispenser.service.DispenserService;
+import dcom.ssdbackend.api.domain.drinker.dto.DrinkerResponseDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(tags = {"Dispenser Controller"})
+@RestController
+@RequiredArgsConstructor
 public class DispenserController {
+
+    private final DispenserService dispenserService;
+
+    @ApiOperation("디스펜서의 정보 조회")
+    @GetMapping("/dispenser")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<DispenserResponseDto.DispenserInfo> dispenserInfo() {
+        return ResponseEntity.ok(dispenserService.dispenserInfo());
+    }
+
+    @ApiOperation("디스펜서 시작")
+    @PostMapping("/dispenser")
+    public ResponseEntity<Void> dispenserStart() {
+        dispenserService.dispenserStart();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+    @ApiOperation("디스펜서 중지")
+    @DeleteMapping("/dispenser")
+    public ResponseEntity<Void> dispenserStop() {
+        dispenserService.dispenserStop();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
 }
--- src/main/java/dcom/ssdbackend/api/domain/dispenser/dto/DispenserResponseDto.java
+++ src/main/java/dcom/ssdbackend/api/domain/dispenser/dto/DispenserResponseDto.java
@@ -1,4 +1,31 @@
 package dcom.ssdbackend.api.domain.dispenser.dto;
 
+import dcom.ssdbackend.api.domain.dispenser.Dispenser;
+import dcom.ssdbackend.api.domain.drinker.Drinker;
+import dcom.ssdbackend.api.domain.drinker.dto.DrinkerResponseDto;
+import io.swagger.annotations.ApiModel;
+import lombok.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DispenserResponseDto {
+
+    @ApiModel(value = "디스펜서 정보 조회")
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DispenserInfo {
+        private String id;
+        private Boolean started;
+
+        public static DispenserResponseDto.DispenserInfo of(Dispenser dispenser) {
+            return DispenserInfo.builder()
+                    .id(dispenser.getId())
+                    .started(dispenser.isStarted())
+                    .build();
+        }
+    }
 }
